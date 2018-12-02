92f35bb93b6629d778b19e57cf4844307d81b4e5
johansundstrom <johan.sundstrom@mdh.se>
2018-12-02 22:03:01 +0100

    meck med att hitta temp

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var request = require("request")
 var UT = require("unixtimejs");
 
 var host = "https://opendata-download-metobs.smhi.se"
+var host = "https://opendata-download-metfcst.smhi.se"
 var qs = "/api/version/1.0/parameter/1/station/96190/period/latest-day/data.json"
+var qs = "/api/category/pmp3g/version/2/geotype/point/lon/16.468774/lat/59.415067/data.json"
 var myBody = {}
 
-url = host + qs
+var url = host + qs
 
 /*
 // ES6
@@ -21,6 +23,23 @@ request( { url: url, json: true},
 
   if (!err && res.statusCode === 200) {
 
+    for (var i = 0; i < body.timeSeries.length; i++)
+    {
+      if (body.timeSeries[0].parameters[i].name === 't')
+      {
+        console.log(body.timeSeries[0].parameters[i].value)
+      }
+    }
+    
+    
+    /*
+    body.timeSeries[0].parameters[1].values.forEach(function(element){
+      console.log(element)
+    })
+    console.log(body.timeSeries[0].parameters[1].name)
+    //console.log(body.timeSeries[0].parameters[1].values[0])
+    */
+
       /*
       // ES6
       body.value.forEach(element => {
@@ -31,12 +50,14 @@ request( { url: url, json: true},
       });
       */
 
+      /*
       body.value.forEach(function(element){
         var utcString = UT.toLocaleString(element.date / 1000)
         var temp = element.value
 
         console.log(utcString + " --> " + temp + "℃")
-      }) 
+      })
+      */ 
     
   }
 })
