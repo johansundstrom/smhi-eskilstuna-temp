e009cd724d2da24935a4a56dad1c85b8022d9138
johansundstrom <johan.sundstrom@mdh.se>
2018-12-04 22:30:44 +0100

    let

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var request = require("request")
+let request = require("request")
 
 let host = "https://opendata-download-metfcst.smhi.se"
 let qs = "/api/category/pmp3g/version/2/geotype/point/lon/16.468774/lat/59.415067/data.json"
@@ -10,14 +10,17 @@ request( {url: smhiUrl, json: true},
     if (!err && res.statusCode === 200) 
     {
 
-/*       for (let i = 0; i < body.timeSeries[0].parameters.length; i++) {
+      /*
+      for (let i = 0; i < body.timeSeries[0].parameters.length; i++) {
       
         if (body.timeSeries[0].parameters[i].name == 't') {
           console.log(body.timeSeries[0].parameters[i].values[0])
         }
         
-      } */
-      for (var i = 0; i < body.timeSeries[0].parameters.length; i++)
+      } 
+      */
+
+      for (let i = 0; i < body.timeSeries[0].parameters.length; i++)
       {
         if (body.timeSeries[0].parameters[i].name == 't')
         {
